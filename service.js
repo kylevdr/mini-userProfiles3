faf5ef8ac28e596b2d0db4985520ffbcfd193b68
Kyle VanDeRiet <kylevdr@gmail.com>
2016-11-17 12:31:07 -0700

    Project complete

--- service.js
+++ service.js
@@ -1,10 +1,19 @@
-angular.module('userProfiles').service('mainService', function($http) {
+angular.module('userProfiles').service('mainService', function($http, $q) {
 
   this.getUsers = function() {
-    return $http({
+    var defer = $q.defer();
+    $http({
         method: 'GET',
         url: 'http://reqres.in/api/users?page=1'
     })
+    .then(function(result) {
+      var parsedResult = result.data.data;
+      parsedResult.map(function(item){
+        item.first_name = 'Ralf';
+      })
+      defer.resolve(parsedResult);
+    });
+    return defer.promise;
   }
 
 });
